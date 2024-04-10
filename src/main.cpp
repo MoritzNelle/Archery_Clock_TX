de2255fc73c5c5b149f169ae8a822139f4c70287
Moritz <152723862+CropCoder@users.noreply.github.com>
2024-04-10 20:39:10 +0200

    Update: updateGroups(), displayGroup(); Validation; first funtional Version

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
 // COMPETITION VARIABLES (meant to be changed by the user)
-//#define numRounds       10    // 
+#define numRounds       10    // 
 //#define numArrows       3
 #define secGetToLine    10    // time to get to the shootingline before the shooting starts
 #define secShooting     90    // time for shooting
 #define numGroups       4     // min. 1, max 4 groups
-#define maxBrightness   255   // max 0-255 (0 = off, 255 = full brightness)
+#define maxBrightness   20   // max 0-255 (0 = off, 255 = full brightness)
 
 //CLOCK VARIABLES (only meant to be changed by the user if a new/diffrent clock is used AND the user knows what they are doing)
 unsigned int colorOfGroups[4] = {0, 21845, 10922, 43681};  // group 1/A: red, group 2/B: green, group 3/C: yellow, group 4/D: blue
 int colorOfTimer              = 21845;  // green //enter color in HSV format (0-65535);
 int colorOfGetToLine          = 43681;  // blue  //enter color in HSV format (0-65535);
 #define NUM_PIXELS              18
-float numLedGroupIndication =   3;
+float numLedGroupIndication =   2;
+float numLedNextGroup      =    1;
 #define numLedGap               1
-int numLedTimer = NUM_PIXELS - numLedGroupIndication - numLedGap;
+int numLedTimer = NUM_PIXELS - numLedGroupIndication - numLedNextGroup - numLedGap -1;
+int listOfGroups[numGroups];
 
 //SYSTEM VARIABLES (NO TOUCHY TOUCHY!...NEVER! otherwise you will break the code and the world will end...or something like that)
 bool HoldState = false;
 bool FFWState =  false;
 int currentGroup = 1;
 int nextGroup = 1;
-int actRound = 0;
+int actRound = 1;
 unsigned long timerOne = 0;
 
 
@@ -69,7 +71,7 @@ void checkButtons(){
 void countDown(float firstPixel, float lastPixel, int color /*HSV*/ ,unsigned long duration){
 
   int iterations = log(maxBrightness) / log(1.05);
-  float numPixels = lastPixel - firstPixel + 1;
+  float numPixels = lastPixel - firstPixel;
   float fadeDelay = (duration*1000 / iterations)/numPixels; // delay for each brightness level; 1/numLedGroupIndication = 0 ... WHY?
   
   for (int i = firstPixel; i <= lastPixel; i++) {
@@ -154,42 +156,75 @@ void hold(){
 }
 
 void updateGroups(){
-  if (actRound == 0){
-
+  int temp = listOfGroups[0];
+  for (int i = 0; i < numGroups - 1; i++) {
+    listOfGroups[i] = listOfGroups[i + 1];
   }
+  listOfGroups[numGroups - 1] = temp;
 }
-  
+
+
+void displayGroup(int currentGroup){
+  for (int i = NUM_PIXELS-numLedGroupIndication; i < NUM_PIXELS; i++) {
+    clock1.setPixelColor(i, clock1.ColorHSV(colorOfGroups[listOfGroups[0]-1], 255, maxBrightness));
+  }
+  clock1.show();
+
+  for (int j = NUM_PIXELS-numLedGroupIndication-numLedNextGroup; j < NUM_PIXELS-numLedGroupIndication; j++) {
+    clock1.setPixelColor(j, clock1.ColorHSV(colorOfGroups[listOfGroups[1]-1], 255, maxBrightness));
+  }
+
+Serial.print("currentGroup: ");Serial.println(currentGroup);
+
+  if (currentGroup == numGroups) {
+    for (int k = NUM_PIXELS-numLedGroupIndication-numLedNextGroup; k < NUM_PIXELS-numLedGroupIndication; k++) {
+      clock1.setPixelColor(k, clock1.ColorHSV(colorOfGroups[listOfGroups[2]-1], 255, maxBrightness));
+    }
+  }
+
+  clock1.show();
+
+}
 
 //--------------------------------------------------------------------------------
 
 void setup() {
+
+  for (int i = 0; i < numGroups; i++) {
+    listOfGroups[i] = i+1;
+  } 
+
   //delay(3000); // 3 second delay for recovery
   clock1.begin();        // initialize NeoPixel strip object
   Serial.begin(9600);    // initialize serial communication at 9600 bits per second
 
   pinMode(FFWbutton, INPUT_PULLUP);   // both buttons are connected to GROUND (not VCC) and pulled up with internal pullup resistors
-  pinMode(Holdbutton, INPUT_PULLUP);  
+  pinMode(Holdbutton, INPUT_PULLUP);
+
+  if (numGroups < 1) {
+    nextGroup = 2;
+  }
+
+  //hold();         // holds every thing on start up until the hold- or continue-button is pressed for the first time
 }
 
 void loop() {
-  actRound++;
   //checkButtons();
-  updateGroups();
-  countDown(0,numLedTimer, colorOfGetToLine, secGetToLine); // get to the line
-  countDown(0,numLedTimer, colorOfTimer, secShooting);      // shooting
-  fade(0);                                              // indicates the end of shooting 
-  hold(); // holds every thing until the hold- or continue-button is pressed, time to collect the arrows and write down the scores
+  while (actRound <= numRounds) {
+    Serial.print("actRound: ");Serial.println(actRound);  // print the actual round for debugging
 
-  
+    for (int i = 0; i < numGroups; i++) {
+      for(int j = 0; j < (sizeof(listOfGroups)/sizeof(listOfGroups[0])); j++) {Serial.print(listOfGroups[j]);}Serial.println(); // print the list of groups for debugging
 
+      displayGroup(i+1);
+      countDown(0,numLedTimer, colorOfGetToLine, secGetToLine); // get to the line
+      countDown(0,numLedTimer, colorOfTimer, secShooting);      // shooting
+      updateGroups();
+    }
+    updateGroups();
+    fade(0);                                                // indicates the end of shooting 
+    hold(); // holds every thing after the last group shoot until the hold- or continue-button is pressed, time to collect the arrows and write down the scores
+    actRound++;
+  }
+  //competition is over
 }
-
-/*
-- larger/wider <---
--- 
-- buzzer
-- brighter
-- more stirty
-- more robust
-- 
-*/
